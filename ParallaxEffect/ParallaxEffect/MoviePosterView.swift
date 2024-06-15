27d2843f550aa2aa13cbbbd1117fd2d56341ed9d
Anup D'Souza <anupdsouza@gmail.com>
2024-06-15 20:51:34 +0530

    Update social links

--- ParallaxEffect/ParallaxEffect/MoviePosterView.swift
+++ ParallaxEffect/ParallaxEffect/MoviePosterView.swift
@@ -3,6 +3,10 @@
 //  ParallaxEffect
 //
 //  Created by Anup D'Souza on 14/06/24.
+//  🕸️ https://www.anupdsouza.com
+//  🔗 https://twitter.com/swift_odyssey
+//  👨🏻‍💻 https://github.com/anupdsouza
+//  ☕️ https://www.buymeacoffee.com/anupdsouza
 //
 
 import SwiftUI
